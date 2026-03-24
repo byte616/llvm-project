f7fa5da411066e37785772d9ed62b7ed17586752
byte616 <p500297@gmail.com>
2026-03-24 10:16:05 +0100

    Register BlockGridDimAnalysis and SharedMemPad passes

--- llvm/lib/Passes/PassBuilder.cpp
+++ llvm/lib/Passes/PassBuilder.cpp
@@ -316,6 +316,8 @@
 #include "llvm/Transforms/Vectorize/LoopVectorize.h"
 #include "llvm/Transforms/Vectorize/SLPVectorizer.h"
 #include "llvm/Transforms/Vectorize/VectorCombine.h"
+#include "llvm/Transforms/Utils/BlockGridDimensionAnalysis.h"
+#include "llvm/Transforms/Utils/SharedMemPad.h"
 #include <optional>
 
 using namespace llvm;
